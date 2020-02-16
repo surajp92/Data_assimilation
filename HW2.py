dda6ca6b3a7c2fb0924bdf35349b3c3f101fec90
Suraj <surajp92@vt.edu>
2020-02-16 16:50:36 -0600

    lorenz equation problem 3 started

--- HW2.py
+++ HW2.py
@@ -86,3 +86,154 @@ for p in range(max_iter):
         break
     
     xold = xnew
+
+#%%
+sigma = 10.0
+rho = 28.0
+beta = 8/3
+dt = 0.01
+ttrain = 2.0
+tmax = 5.0
+freq = 1
+mu = 0.0
+var = 0.09
+std = np.sqrt(var)
+max_iter = 50
+tolerance = 1e-6
+lr = 0.1
+
+X_init = np.ones(3)
+X_da_init = 1.1*np.ones(3)
+nttrain = int(ttrain/dt)
+ntmax = int(tmax/dt)
+nobs = int(nttrain/freq)
+t = np.linspace(0,ttrain,nttrain+1)
+
+ind = [freq*i for i in range(1,int(nttrain/freq)+1)]
+tobs = t[ind]
+
+rk = var*np.eye(3)
+
+f = np.zeros((nobs,3))
+lagr = np.zeros((nobs,3))
+
+xold = X_da_init
+
+def plot_lorenz(X,t,Z,tz):
+    color = ['r','b','g']
+    fig,ax = plt.subplots(nrows=3,ncols=1,figsize=(8,6),sharex=True)
+    axs = ax.flat
+    
+    for k in range(X.shape[1]):
+        axs[k].plot(t,X[:,k],label=r'$x_'+str(k+1)+'$',color=color[k],
+                   linewidth=2)
+        axs[k].plot(tz,Z[:,k],'o',label=r'$z_'+str(k+1)+'$',color=color[k],
+                    fillstyle='none',markersize=8)
+        axs[k].set_xlim(0,t[-1])
+        axs[k].set_ylim(np.min(X[:,k])-5,np.max(X[:,k])+5)
+        axs[k].legend()
+      
+    plt.show()
+       
+def lorenz(X_init,sigma,rho,beta,dt,nt):
+    X = np.zeros((nt+1,3))
+    
+    X[0,0] = X_init[1]; X[0,1] = X_init[1]; X[0,2] = X_init[2]
+    
+    for n in range(1,nt+1):
+        X[n,0] = X[n-1,0] - dt*sigma*(X[n-1,0] - X[n-1,1])
+        X[n,1] = X[n-1,1] + dt*(rho*X[n-1,0] - X[n-1,1] - X[n-1,0]*X[n-1,2])
+        X[n,2] = X[n-1,2] + dt*(X[n-1,0]*X[n-1,1] - beta*X[n-1,2])
+    
+    return X
+
+def dxk_model(sigma,rho,beta,dt,xk):
+    dxk_m = np.zeros((3,3))
+    dxk_m[0,0] = 1 - sigma*dt
+    dxk_m[0,1] = -dt
+    dxk_m[1,0] = rho*dt
+    dxk_m[1,1] = 1 - dt
+    dxk_m[1,2] = -dt*xk[0]
+    dxk_m[2,0] = dt*xk[1]
+    dxk_m[2,2] = 1-beta*dt
+    
+    return dxk_m
+        
+Xtrue = lorenz(X_init,sigma,rho,beta,dt,nttrain)
+
+xobs = Xtrue[ind]
+zobs = xobs + np.random.normal(mu,std,[nobs,3])
+
+for p in range(max_iter):
+    Xda = lorenz(xold,sigma,rho,beta,dt,nttrain)
+    
+    xdaobs = Xda[ind]
+    for k in range(nobs):
+        xk = xdaobs[k,:].reshape(-1,1)
+        dxk_m = dxk_model(sigma,rho,beta,dt,xk)
+        fk = np.linalg.multi_dot([dxk_m.T, np.linalg.inv(rk), xk])
+        f[k,:] = fk.flatten()
+        
+    lagr[-1] = f[-1,:]
+    
+    for k in range(nobs-2,-1,-1):
+        xk = xdaobs[k,:].reshape(-1,1)
+        lkp = lagr[k+1].reshape(-1,1)
+        dxk_m = dxk_model(sigma,rho,beta,dt,xk)
+        lk = np.dot(dxk_m.T, lkp)
+        lagr[k,:] = lk.flatten()
+    
+    x0 = xdaobs[0,:].reshape(-1,1)
+    dx0_m = dxk_model(sigma,rho,beta,dt,x0)
+    grad = -np.dot(dx0_m.T,lagr[0,:].reshape(-1,1))
+    
+    xnew = xold - lr*grad.flatten()/np.linalg.norm(grad)
+    
+    print(p, ' ', xold, ' ' , xnew, ' ', np.linalg.norm(xnew-xold))
+    
+    if np.linalg.norm(xnew-xold) < tolerance:
+        break
+    
+    xold = xnew
+    
+plot_lorenz(Xtrue,t,zobs,tobs)
+
+#X = lorenz(X_init,sigma,rho,beta,dt,ntmax)
+#plot_lorenz(X,tmax,ntmax)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
